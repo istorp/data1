481e42791b1dbe8c0c4267e3ad9e78ad77a8ad45
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-03-02 14:57:31 +0100

    steg1

---
+++ data1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace data1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("skriv ditt namn");
+
+            string hej = Console.ReadLine();
+            Console.WriteLine(hej);
+        }
+    }
+}
